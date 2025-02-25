e62b2ae79ffffb50053332a1937d2bff7f553e47
shrey <shrey.sadhukhan21@gmail.com>
2025-02-25 16:46:54 +0530

    done

--- app/university/[slug]/page.tsx
+++ app/university/[slug]/page.tsx
@@ -184,14 +184,14 @@ export default function UniversityPageDetails({
                         <p>
                           <strong> Session:</strong> {program.session}
                         </p>
-                        <button className="bg-orange-500 text-white  text-center py-2 px-4 rounded-md mt-4 hover:bg-orange-600">
+                        {/* <button className="bg-orange-500 text-white  text-center py-2 px-4 rounded-md mt-4 hover:bg-orange-600">
                           <Link
-                            // href={`/university/${university.visit}/${program.visit}`}
+                            href={`/university/${university.visit}/${program.visit}`}
                             href={`#`}
                           >
                             View Details
                           </Link>
-                        </button>
+                        </button> */}
                       </div>
 
                       {/* View Details Button */}

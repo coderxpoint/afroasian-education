5b755eb6e7b2c210d1a9ce73dc109914b8fab773
shrey <shrey.sadhukhan21@gmail.com>
2025-02-26 12:22:29 +0530

    working on course page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
       <Steps />
       <StudentSaying />
       <TestimonialsSection />
-      {/* <BlogsHome /> */}
+      <BlogsHome />
       <CallToAction />
     </div>
   );
